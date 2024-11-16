cbceec778815367f83fee9e873d496a60f7f20b2
sofiapetruk <sofiapetruk900@gmail.com>
2024-11-15 22:17:50 -0300

    colocação de todas as tipagem que  temos no java

--- rede-alva/src/types/types.ts
+++ rede-alva/src/types/types.ts
@@ -1,6 +1,45 @@
 export type TipoComunidade = {
-    tipoComunidade: string;
+    idComunidade?:number; 
+    tipoComunidade:string;
     endereco: string;
     estado: string;
     totalEnergia?: number; //opcional
 };
+
+export type TipoUnidade = {
+    idUnidade?:number;
+    idComunidade?:number;
+    numeroUnidade?:number;
+    nomeUnidade:string;
+    capacidadeGeracao?:number;
+    capacidadeConsumo?:number;
+    saldoEnergia?:number;
+}
+
+export type TipoComercio = {
+    idComercio?:number;
+    idComunidade:number;
+    unidadeVedendoraId: number;
+    unidadeCompradoraId:number;
+    tipoTransacao:string;
+    quantidade:number;
+    saldoVendedor?:number;
+    saldoComprador?:number;
+    dataHora:string;
+}
+
+export type TipoArmazenamento = {
+    idArmazenamento?:number;
+    idComunidade:number;
+    idUnidade:number;
+    tipoGeracao:string;
+    quantidade:string;
+}
+
+export type TipoUser = {
+    idCadastro:number;
+    user:string;
+    email:string;
+    senha:string;
+    date:string;
+}
