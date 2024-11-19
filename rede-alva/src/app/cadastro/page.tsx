1440eed0fc879dc504163cb514d44f8f5aa7a1e4
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2024-11-19 12:01:52 -0300

    Ajustando CSS na página cadastro-comunidade/unidade

--- rede-alva/src/app/cadastro/page.tsx
+++ rede-alva/src/app/cadastro/page.tsx
@@ -18,29 +18,32 @@ export default function Cadastro() {
   if (!isAuthenticated) return null; // Evita renderizar antes de redirecionar
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">Cadastro</h1>
-      <p className="mt-2 text-gray-600">
-        Escolha uma das opções abaixo para continuar o cadastro:
-      </p>
-      <div className="mt-4 space-y-4">
-        <Link href="/comunidades/cad-comunidades">
-          <button className="px-4 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700">
-            Cadastrar Comunidade
-          </button>
-        </Link>
-        <Link href="/unidades/cad-unidades">
-          <button className="px-4 py-2 text-white bg-green-600 rounded hover:bg-green-700">
-            Cadastrar Unidade
-          </button>
-        </Link>
+    <div className="container-cadastropage">
+      <div className="fundo">
+        <h1>Cadastro</h1>
+        <p>
+          Escolha uma das opções abaixo para continuar o cadastro:
+        </p>
+        <div>
+          <Link href="/comunidades/cad-comunidades">
+            <button className='botao'>
+              Cadastrar Comunidade
+            </button>
+          </Link>
+          <Link href="/unidades/cad-unidades">
+            <button className='botao'>
+              Cadastrar Unidade
+            </button>
+          </Link>
+        </div>
+        <button
+          onClick={logout}
+          className="mt-8 px-4 py-2 text-black bg-gray-200 rounded hover:bg-red-700"
+        >
+          Sair
+        </button>
       </div>
-      <button
-        onClick={logout}
-        className="mt-8 px-4 py-2 text-white bg-red-600 rounded hover:bg-red-700"
-      >
-        Logout
-      </button>
+
     </div>
   );
 }
