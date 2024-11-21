63d99a85b6f3316fc4f8c1f75ae0015dbeb7da41
sofiapetruk <sofiapetruk900@gmail.com>
2024-11-21 09:00:06 -0300

    arrumando a parte do cadastro

--- rede-alva/src/app/comercios/cad-comercios/page.tsx
+++ rede-alva/src/app/comercios/cad-comercios/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TipoComercio, TipoComunidade } from "@/types/types";
+import { TipoComercio} from "@/types/types";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
