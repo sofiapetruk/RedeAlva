7c0557a208ff9ebe58c161d9e007b33d1d4e399e
jliamonteiro <jlmonteiro2355@gmail.com>
2024-11-22 04:57:40 -0300

    inserção das fotos dos integrantes e informações

--- rede-alva/src/app/page.tsx
+++ rede-alva/src/app/page.tsx
@@ -6,6 +6,11 @@ import casa from "@/img/casa.png"
 import check from "@/img/Check.png"
 import paineis from "@/img/paineis.png"
 import Link from "next/link";
+import Monteiro from "@/img/monteiro.jpg"
+import Petruk from "@/img/petruk.jpg"
+import Github from "@/img/github.png"
+import Carlos from "@/img/Carlos.jpeg"
+
 
 export default function Home () {
   return (
@@ -143,8 +148,43 @@ export default function Home () {
             alt="Casa"
           />
         </div>
-
       </div>
+        <div className="integrantes-container">
+          <div className="integrante">
+              <Image src={Monteiro} alt="Julia Monteiro" className="imagem" width={200}
+            height={200} />
+              <p className="nome">Julia Monteiro</p>
+              <p className="rm">RM:557023</p>
+              <p className="curso">1TDSPH</p>
+              <a href="https://github.com/jliamonteiro" className="github-link" target="_blank">
+                  <Image src={Github} alt="Ícone github" className="github-icon" width={50}
+            height={50}/>
+              </a>
+          </div>
+          <div className="integrante">
+              <Image src={Petruk} alt="Sofia Petruk" className="imagem" width={200}
+            height={200}/>
+              <p className="nome">Sofia Andrade Petruk</p>
+              <p className="rm">RM:556585</p>
+              <p className="curso">1TDSPH</p>
+              <a href="https://github.com/sofiapetruk" className="github-link" target="_blank">
+                  <Image src={Github} alt="Ícone github" className="github-icon" width={50}
+            height={50}/>
+              </a>
+          </div>
+          <div className="integrante">
+              <Image src={Carlos} alt="Carlos Eduardo" className="imagem" width={200}
+            height={200} />
+              <p className="nome">Carlos Eduardo Rabelo</p>
+              <p className="rm">RM:558470</p>
+              <p className="curso">1TDSPH</p>
+              <a href="https://github.com/Victor-Estrella" className="github-link" target="_blank">
+                  <Image src={Github} alt="Ícone github" className="github-icon" width={50}
+            height={50} />
+              </a>
+          </div>
+        </div>
+      
     </div>
   )
 }
